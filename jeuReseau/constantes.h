b991db59f1e977d861d83db1da4fff9335d19c75
paul <pmc.valentin@gmail.com>
2015-10-24 15:05:50 +0200

    [add] folder with L1 game

---
+++ jeuReseau/constantes.h
@@ -0,0 +1,25 @@
+/**************************************************************************************/
+/**                                    Jeu :                                         **/
+/**                            - Par Paul VALENTIN                                   **/
+/**                            - Version : Alpha 1.0                                 **/
+/**                            - sans IA                                             **/
+/**                            - Modif : 21/12/2013                                  **/
+/**************************************************************************************/
+
+
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+#define NB_BLOCS_HAUTEUR    15
+#define NB_BLOCS_LARGEUR    20
+#define TAILLE_BLOC         50
+#define HAUTEUR             (TAILLE_BLOC * NB_BLOCS_HAUTEUR)
+#define LARGEUR             (TAILLE_BLOC * NB_BLOCS_LARGEUR) + 60
+
+enum{SINGE_FACE = 0,SINGE_DOS = 1,SINGE_DROITE = 2,SINGE_GAUCHE = 3};
+enum{MONSTRE_FACE = 0, MONSTRE_DOS = 1, MONSTRE_DROITE = 2, MONSTRE_GAUCHE = 3};
+enum{VIDE = 0,ROCHER = 1,CLEF = 2,OR = 3,COFFRE = 4,MONSTRE = 5, PIEGE = 6, PERSONNAGE = 7};
+enum{BAS = 0,HAUT = 1,GAUCHE = 2,DROITE = 3};
+
+
+#endif // CONSTANTES_H_INCLUDED
---
+++ jeuReseau/jeu.h
@@ -0,0 +1,5 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+
+
+#endif // JEU_H_INCLUDED
