8a61dc4c4fed17b9210317e45669cb276a3bc0ba
Sephage <pmc.valentin@gmail.com>
2015-12-30 18:27:45 +0100

    modification des fonctions d'initialisation client et serveur

--- jeuReseau/server/initGame.c
+++ jeuReseau/server/initGame.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL/SDL.h>
-
+#include "../joueur.h"
 
 /*Function which initialize the map with the good character and the good map
  * TODO :	change the file with a string
@@ -10,30 +9,6 @@
 void initGame(Character character[2], int map[][]){
 	string file = "map/niveau2.map";
 	int i=0, j=0, k=0;
-	character[0]->character[BAS] = SDL_LoadBMP("sprites/BomberFront.bmp");
-	character[0]->character[HAUT] = SDL_LoadBMP("sprites/BomberBack.bmp");
-	character[0]->character[DROITE] = SDL_LoadBMP("sprites/BomberRight.bmp");
-	character[0]->character[GAUCHE] = SDL_LoadBMP("sprites/BomberLeft.bmp");
-	character[0]->actualCharacter = character[HAUT];
-	
-	SDL_SetColorKey(character[0]->character[BAS], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[BAS]->format, 255, 255, 255));
-	SDL_SetColorKey(character[0]->character[HAUT], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[HAUT]->format, 255, 255, 255));
-	SDL_SetColorKey(character[0]->character[DROITE], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[DROITE]->format, 255, 255, 255));
-	SDL_SetColorKey(character[0]->character[GAUCHE], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[GAUCHE]->format, 255, 255, 255));
-
-
-
-	/*Load different pictures here*/
-	character[1]->character[BAS] = SDL_LoadBMP("sprites/BomberFront.bmp");
-	character[1]->character[HAUT] = SDL_LoadBMP("sprites/BomberBack.bmp");
-	character[1]->character[DROITE] = SDL_LoadBMP("sprites/BomberRight.bmp");
-	character[1]->character[GAUCHE] = SDL_LoadBMP("sprites/BomberLeft.bmp");
-	character[1]->actualCharacter = character[HAUT];
-
-	SDL_SetColorKey(character[1]->character[BAS], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[BAS]->format, 255, 255, 255));
-	SDL_SetColorKey(character[1]->character[HAUT], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[HAUT]->format, 255, 255, 255));
-	SDL_SetColorKey(character[1]->character[DROITE], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[DROITE]->format, 255, 255, 255));
-	SDL_SetColorKey(character[1]->character[GAUCHE], SDL_SRCCOLORKEY, SDL_MapRGB(character[0]->character[GAUCHE]->format, 255, 255, 255));
 
 
 	initCarte(map, file);
